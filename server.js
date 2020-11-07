475ce217d233fb7da5b80fe611c076fdd8bb3a80
MillanUka <ukamillan@gmail.com>
2020-11-07 23:08:31 +1300

    Setup express

---
+++ server.js
@@ -0,0 +1,28 @@
+const path = require("path");
+const express = require("express");
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+const buildPath = path.join(__dirname, "..", "build");
+app.use(express.static(buildPath));
+
+if (process.env.NODE_ENV === "production") {
+  // Exprees will serve up production assets
+  app.use(express.static("../frontend/build"));
+
+  // Express serve up index.html file if it doesn't recognize route
+  const path = require("path");
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+  });
+}
+
+var router = express.Router();
+//router.use("/evidence", require("./routes/Evidence"));
+
+app.use("/api", router);
+
+app.listen(PORT, () => {
+  console.log(`server started on port ${PORT}`);
+});
